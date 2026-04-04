bd7d1601b225729d9560fed909d602177488bdbb
NJ Amanturov 4580 <namanturov@megacom.kg>
2026-04-04 19:04:49 +0600

    as

--- order-service/src/main/java/com/example/orderservice/integration/client/PaymentClient.java
+++ order-service/src/main/java/com/example/orderservice/integration/client/PaymentClient.java
@@ -2,7 +2,6 @@ package com.example.orderservice.integration.client;
 
 import com.example.orderservice.config.feign.PaymentClientConfig;
 import com.example.orderservice.dto.business.PaymentHttpHeader;
-import com.example.orderservice.exception.ExternalIntegrationServiceException;
 import com.example.orderservice.exception.RateLimitExceededException;
 import com.example.orderservice.exception.ServiceUnavailableException;
 import com.example.orderservice.exception.TooManyConcurrentRequestsException;
@@ -15,7 +14,6 @@ import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -32,19 +30,14 @@ public interface PaymentClient {
     @CircuitBreaker(name = "paymentClient", fallbackMethod = "createFallbackOnCircuitBreaker")
     @RateLimiter(name = "paymentClient", fallbackMethod = "createFallbackOnRateLimiter")
     @Bulkhead(name = "paymentClient", fallbackMethod = "createFallbackOnBulkhead")
-    @Retry(name = "paymentClient", fallbackMethod = "createFallbackOnRetry")
-    ResponseEntity<Void> create(@RequestHeader(PaymentHttpHeader.IDEMPOTENCY)
-                                UUID idempotencyKey,
-                                @RequestBody
-                                CreatePaymentRequest request);
+    @Retry(name = "paymentClient")//и тестируя понял что для него и не нужен fallbackMethod.
+    void create(@RequestHeader(PaymentHttpHeader.IDEMPOTENCY)
+                UUID idempotencyKey,
+                @RequestBody
+                CreatePaymentRequest request);
 
     //ну по идеи можно было на некоторые redirect на чет другие сервисы. Аля с кеша или тип того, но было лень думать,
     //потому чисто exceptions выкидываю. Ну а так дааа я пон что можно туда сюда делать.
-    default void createFallbackOnRetry(UUID idempotencyKey,
-                                       CreatePaymentRequest request,
-                                       ExternalIntegrationServiceException ignored) {
-        throw new ServiceUnavailableException("Payment-service временно недоступен, попробуй позже");
-    }
 
     default void createFallbackOnBulkhead(UUID idempotencyKey,
                                           CreatePaymentRequest request,
--- order-service/src/test/java/com/example/orderservice/integration/client/PaymentClientTest.java
+++ order-service/src/test/java/com/example/orderservice/integration/client/PaymentClientTest.java
@@ -1,11 +1,14 @@
 package com.example.orderservice.integration.client;
 
+import com.example.orderservice.exception.EntityNotFoundException;
+import com.example.orderservice.exception.ExternalIntegrationServiceException;
 import com.example.orderservice.integration.dto.enums.CurrencyType;
 import com.example.orderservice.integration.dto.request.CreatePaymentRequest;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.wiremock.spring.ConfigureWireMock;
 import org.wiremock.spring.EnableWireMock;
@@ -13,8 +16,8 @@ import org.wiremock.spring.EnableWireMock;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -31,15 +34,44 @@ class PaymentClientTest {
     @Autowired
     private PaymentClient paymentClient;
 
+    //    все тесты прошли.
     @Test
-    void testCreateOrderSuccess() {
+    void testCreatePaymentSuccess() {
         var idempotencyKey = UUID.randomUUID();
-        //ну предположим что заказ мы как то да и нашли...утаим как, но нашли.
-        var orderId = UUID.randomUUID();
-        var req = CreatePaymentRequest.of(orderId, BigDecimal.TEN, CurrencyType.KGS);
-        ResponseEntity<Void> response = paymentClient.create(idempotencyKey, req);
+        var orderId = UUID.fromString("1131912d-67c2-4ec8-b4d3-61bff77f6f73");
 
-        verify(postRequestedFor(urlEqualTo("/payments")));
-        assertEquals(201, response.getStatusCode().value());
+        var req = CreatePaymentRequest.of(orderId, BigDecimal.TEN, CurrencyType.KGS);
+
+        paymentClient.create(idempotencyKey, req);
+
+        WireMock.verify(postRequestedFor(urlEqualTo("/payments")));
+    }
+
+    @Test
+    void testCreatePaymentNotFound() {
+        var idempotencyKey = UUID.randomUUID();
+        var orderId = UUID.fromString("69d0f3ad-d7f8-8329-a327-ccdaa44d2d2d");
+
+        var req = CreatePaymentRequest.of(orderId, BigDecimal.TEN, CurrencyType.KGS);
+
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            paymentClient.create(idempotencyKey, req);
+        });
+
+        WireMock.verify(postRequestedFor(urlEqualTo("/payments")));
+    }
+
+    @Test
+    void testCreatePaymentBadRequest() {
+        var idempotencyKey = UUID.randomUUID();
+        var orderId = UUID.fromString("efe62388-10af-4094-b27e-b3a92f7cb887");
+
+        var req = CreatePaymentRequest.of(orderId, BigDecimal.TEN, CurrencyType.KGS);
+
+        Assertions.assertThrows(ExternalIntegrationServiceException.class, () -> {
+            paymentClient.create(idempotencyKey, req);
+        });
+
+        WireMock.verify(postRequestedFor(urlEqualTo("/payments")));
     }
 }
