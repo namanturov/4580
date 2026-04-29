60c5142d52fa0c51912a7dfe8cfde066096f8c57
Amanturov Nazar <amanturovnazarbek4580@mail.ru>
2026-04-29 21:17:21 +0600

    task-11 / done

--- order-service/src/main/java/com/example/orderservice/api/header/PaymentHttpHeader.java
+++
@@ -1,9 +0,0 @@
-package com.example.orderservice.api.header;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class PaymentHttpHeader {
-    public static final String IDEMPOTENCY = "X-Idempotency-Key";
-}
